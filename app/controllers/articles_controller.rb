912a2bfe21001a125c870415251fb8c94a646c41
Rich <richlow@gmail.com>
2016-07-14 13:43:56 +1000

    add route

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,19 @@
+class ArticlesController <ApplicationController
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    #render plain: params[:article].inspect #displays
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_show(@article)
+  end  
+  
+  private
+    def article_params
+      params.required(:article).permit( :title, :description)
+    end
+    
+ 
+end

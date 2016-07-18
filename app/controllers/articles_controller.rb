79f5940164de7c0509f89d8bfa267543d7161612
richlow <richlow@gmail.com>
2016-07-18 05:03:07 +0000

    removed the validation on description - it is now working...

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,29 @@
-class ArticlesController <ApplicationController
+class ArticlesController < ApplicationController
   def new
-    @article = Article.new
+    @articles = Article.new
   end
-  
+
   def create
     #render plain: params[:article].inspect #displays
-    @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
-  end  
-  
+    @articles = Article.new(article_params)
+    #@article.save
+    #redirect_to article_path(@article)
+    if @articles.save
+      flash[:notice] = "Artice was successfully created"
+      redirect_to article_path(@articles)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @articles = Article.find(params[:id])
+  end
+
   private
     def article_params
-      params.required(:article).permit( :title, :description)
+      params.require(:article).permit(:title, :description)
+
     end
-    
- 
+
 end
